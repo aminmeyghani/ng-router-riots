c9c02e54ec7540dd682748dc3276f93eb5e340b6
Amin Meyghani <meyghania@gmail.com>
2015-07-22 13:56:57 -0400

    clean up the todo store

--- app/todo.store.js
+++ app/todo.store.js
@@ -14,45 +14,6 @@ function TodoStore() {
     { title: 'tt', done: false }
   ];
 
-  // this.xlisten = (e, fn) => {
-  //   var self = this;
-  //   return {
-  //     then: function (fn) {
-  //       if (fn) {
-  //         self.on(e, fn);
-  //         self.trigger('todos_changed', self.todos);
-  //       } else {
-  //         self.on(e, function () { self.trigger('todos_changed', self.todos) });
-  //       }
-  //     }
-  //   };
-  // };
-
-  /*
-    this.listen('todo_init').then();
-
-    this.listen('todo_add')
-        .then( newTodo => { this.todos.push(newTodo); })
-
-    this.listen('todo_remove')
-        .then(()=>{this.todos.pop(); });
-  */
-
-  // this.listen = (e) => (t) => {
-  //   var self = this;
-
-  //   var broadcast = () => { self.trigger('todos_changed', self.todos); };
-
-  //   if (typeof t === 'string') {
-  //     self.on(e, () => { broadcast(); });
-  //   } else if (typeof t === 'function') {
-  //     self.on(e, t);
-  //     broadcast();
-  //   } else {
-  //     return undefined;
-  //   }
-  // };
-
   this.listen = (e, fn, d, bn) => {
     var self = this;
     var config = {model: d, triggerName: bn ? bn : 'change'};
@@ -66,59 +27,14 @@ function TodoStore() {
   };
 
   this.listen('todo_init', 'broadcast', this.stuff);
-  // this.listen('todo_init', items => {
-  //   this.trigger('change', this.stores)
-  // });
 
   this.listen('todo_add', newTodo => {
     this.stuff.push(newTodo);
   });
+  
   this.listen('todo_remove', () => {
     this.stuff.pop();
   });
-
-
-  // this.listen('todo_init')('broadcast');
-
-  // this.listen('todo_add')(newTodo => {
-  //   this.todos.push(newTodo);
-  // });
-
-  // this.listen('todo_remove')(newTodo => {
-  //   this.todos.pop();
-  // });
-
-  // Our store's event handlers / API.
-  // This is where we would use AJAX calls to interface with the server.
-  // Any number of views can emit actions/events without knowing the specifics of the back-end.
-  // This store can easily be swapped for another, while the view components remain untouched.
-
-  // this.listen('todo_add')('todos_changed', newTodo => {
-  //   this.todos.push(newTodo);
-  // });
-
-
-
-
-    // this.listen('todo_init')
-    //     .then(()=>{}).broadcast();
-
-  // this.listen('todo_add', function (newTodo) {
-  //   this.todos.push(newTodo);
-  // });
-  // this.on('todo_add', newTodo => {
-  //   this.todos.push(newTodo);
-  //   this.trigger('todos_changed', this.todos);
-  // });
-
-  // this.on('todo_remove', () => {
-  //   this.todos.pop();
-  //   this.trigger('todos_changed', this.todos);
-  // });
-
-  // this.on('todo_init', () => {
-  //   this.trigger('todos_changed', this.todos);
-  // });
 
   return store;
   // The store emits change events to any listening views, so that they may react and redraw themselves.
